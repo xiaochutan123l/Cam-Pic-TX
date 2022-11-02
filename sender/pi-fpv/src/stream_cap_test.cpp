179c2f0ede54a8ef35d698a5ba866e055dd317a4
Xiaochu Tan <xiaochutan123@gmail.com>
2022-11-02 15:35:56 +0000

    sychronize code

--- sender/pi-fpv/src/stream_cap_test.cpp
+++ sender/pi-fpv/src/stream_cap_test.cpp
@@ -1,9 +1,21 @@
 #include "cam_controller_impl.h"
 #include "cam_controller.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 uint8_t frame_rate = 30;
+
+int main_arg_count = 2;
+
+std::string info = 
+        "Usage: stream_cap_test [options] [flags]\n\n"
+        "options:\n"
+        "--single      capture a single frame\n"
+        "--multi       capture multiple frames\n"
+        "\nflags:\n"
+        "-o            output file name/base name\n"
+        "-rate         capture frame rate"
 
 void handler_cb(void *start, uint32_t length) {
     printf("handler callback function\n");
@@ -21,11 +33,12 @@ void handler_cb(void *start, uint32_t length) {
 	fclose(out_fp);
 }
 
-int main () {
+void single_frame_test() {
     char *dev_name = (char*)"/dev/video0";
     Stream_fmt fmt = {RESOLUTION_FMT::R_240,
                         frame_rate,
                         IMAGE_FMT::JPEG};
+
     Cam_Controller *cam = new Cam_Controller_Impl(fmt, dev_name);
     cout << "start cam ..." << endl;
     cam->start(handler_cb);
@@ -35,5 +48,36 @@ int main () {
     cam->close();
     cout << "exit" << endl;
     delete cam;
+}
+
+void capture_multiple_frame() {
+    char *dev_name = (char*)"/dev/video0";
+    Stream_fmt fmt = {RESOLUTION_FMT::R_240,
+                        frame_rate,
+                        IMAGE_FMT::JPEG};
+                        
+    Cam_Controller *cam = new Cam_Controller_Impl(fmt, dev_name);
+    cout << "start cam ..." << endl;
+    cam->start(handler_cb);
+    cout << "camera ready, wait for capturing" << endl;
+
+    for (int i = 0; i < count; i++){
+        cam->get_frame();
+    }
+
+    cout << "capture done" << endl;
+    cam->close();
+    cout << "exit" << endl;
+    delete cam;
+
+}
+
+int main (int argc, char *argv[]) {
+    if (argc <= 2) {
+        fprintf(stderr, "Usage: %s <output file> <codec name>\n", argv[0]);
+        exit(0);
+    }
+
+ 
     return 1;
 }

5065f4a8141bbdfee70cde7c120774c024f95edb
xiaochutan123l <44642585+xiaochutan123l@users.noreply.github.com>
2022-11-09 00:08:32 +0100

    add new transport protocol definition
    add new transport protocol header and behaviour definition

--- sender/pi-fpv/src/communication/packet_handler.h
+++ sender/pi-fpv/src/communication/packet_handler.h
@@ -29,6 +29,114 @@ struct chunk_header {
 	uint16_t chunk_len;
 };
 
+/* Messager Header*/
+/*
+Message structure:
+
++-----------------------+
+|   Message Header      |
++-----------------------+
+|   Chunk Header        |
++-----------------------+
+|   Data                |
++-----------------------+
+
+A message consist of Message header, a chunk header(only for audio/video message) and its data.
+
+Message Header
+
+0               1               2               3
+
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++---------------+---------------+------------------------------+
+|  Message Type |     Flag      |        Chunk Number          |
++---------------+---------------+------------------------------+
+|      Total Chunk Number       |        Chunk Length          |
++-------------------------------+------------------------------+
+|                       Frame Sequence Number                  |
++--------------------------------------------------------------+
+
+- Message Type:
+	- Video: This message data is a video frame
+	- Audio: This message data is a audio frame
+	- Ack: This message is a pure ack for the last frame received by receiver
+	- Ack+request: This message contains a normal ack and some bunded requests(control message) 
+- Flag:
+- Chunk Number: count the current chunk number of this message
+- Total Chunk Number: total number of chunks in this message
+- Chunk Length: Total length of this packet/chunk (header included)
+- Frame Sequence Number: individual sequence number of each message(frame), audio and video are separatly
+
+Chunk：Header - Video
+
++---------------+---------------+---------------+--------------+
+|     Format    |  Frame Rate   |     Codec     |   Reserved   |
++---------------+---------------+---------------+--------------+
+|                           Timestamp                          |
++--------------------------------------------------------------+
+
+- Format: Video frame format
+- Frame Rate: Frame rate
+- Codec: encoder name
+- Reserved: for potential useage
+- Timestamp: timestamp of this frame
+
+Chunk Header - Audio
+
++---------------+---------------+---------------+--------------+
+|     Format    |Channel Number |  Sample Rate  |    Depth     |
++---------------+---------------+---------------+--------------+
+|                           Timestamp                          |
++--------------------------------------------------------------+
+
+- Format: Audio frame format
+- Channel Number: channel number of this audio frame
+- Sample Rate: audio frame sample rate [Hz]
+- Depth: sample depth [bit]
+- Timestamp: timestamp of this frame
+
+ACK/ACK_CTL Header
+
+0               1               2               3
+
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++---------------+---------------+---------------+--------------+
+|  Message Type |   Flag        |  Loss Rate    | Chunk Number |
++---------------+---------------+---------------+--------------+
+|     Frame Sequence Number     |         Frame ACK            |
++-------------------------------+------------------------------+
+|          Chunk Length         |        Request Number        |
++-------------------------------+------------------------------+
+|         Request Type 1        |        Request Data 1        |
++-------------------------------+------------------------------+
+|                               .                              |
+|                               .                              |
+|                               .                              |
++-------------------------------+------------------------------+
+|         Request Type N        |        Request Data N        |
++-------------------------------+------------------------------+
+
+An ACK packet will be sent by receiver after receiving each audio or video frame,
+to tell sender some network conditions
+Meanwhile some request for control message can be added to this kind of message for efficiency.
+It will be sufficient for requests which are not latency sensetive.
+If realtime requierd, then set flag to urgen, or use separat wireless module for this kind of realtime message.
+
+- Header: 
+	- Message Type: ACK/ACK_Request
+	- Flag: this ack sent for an audio or video frame
+	- Loss Rate: record the packet loss rate [n of 100, or % number] of last frame
+	- Chunk Number: in order to avoid packet loss because of UDP, multiple ack packet will
+		        be transmitted repeatly (3 suggested), udp server can drop the repeated
+			packet if already received one
+	- Frame Sequence Number: cooresponding frame sequence number of this frame
+	- Frame ACK: acknowlegement, report if the last frame recievd successfully
+	- Chunk Length: the whole packet length include header
+	- Request Number: the number of bunded requests
+
+- Data: A serials of requests.
+*/
+
 enum class sock_type {
 	UDP,
 	TCP,

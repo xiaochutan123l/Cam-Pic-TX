64e59cfb75037b9957d931f84fa87cc8ed8e1a97
Xiaochu Tan <xiaochutan123@gmail.com>
2022-11-13 10:15:27 +0000

    implement unpack function for new header and defined partial header enums

--- sender/pi-fpv/src/communication/packet_handler.cpp
+++ sender/pi-fpv/src/communication/packet_handler.cpp
@@ -1,6 +1,25 @@
 #include "packet_handler.h"
 
 
+void unpack_msg_header(uint8_t * pkt_buffer, struct msg_header_v2 *msg_hdr) {
+    memcpy(msg_hdr, pkd_buffer, MSG_HDR_LEN);
+}
+
+void unpack_chunk_video_header(uint8_t * pkt_buffer, struct chunk_header_video_v2 *chunk_hdr) {
+    memcpy(chunk_hdr, pkd_buffer, CHUNK_HDR_LEN);
+}
+void unpack_chunk_audio_header(uint8_t * pkt_buffer, struct chunk_header_audio_v2 *chunk_hdr) {
+    memcpy(chunk_hdr, pkd_buffer, CHUNK_HDR_LEN);
+}
+
+void unpack_packet_header(uint8_t * pkt_buffer, struct packet_header_v2 *pkd_hdr) {
+    memcpy(pkt_hdr, pkd_buffer, PKT_HDR_LEN);
+}
+
+uint8_t get_msg_type(uint8_t * pkt_buffer) {
+    return pkd_buffer[0];
+}
+
 struct chunk_header unpack_header(uint8_t * pkd_buffer, uint8_t hdr_len){
     struct chunk_header ret_hdr;
     struct chunk_header * hdr_ptr;
@@ -12,16 +31,6 @@ struct chunk_header unpack_header(uint8_t * pkd_buffer, uint8_t hdr_len){
     ret_hdr.chunk_num = hdr_ptr->chunk_num;
     ret_hdr.chunk_len = hdr_ptr->chunk_len;
     return ret_hdr;
-}
-
-void pack_header(uint8_t * buffer, struct chunk_header &hdr){
-    struct chunk_header * hdr_ptr;
-    hdr_ptr = (chunk_header *)buffer;
-    hdr_ptr->type = hdr.type;
-    hdr_ptr->flags = hdr.flags;
-    hdr_ptr->total_chunk_num = hdr.total_chunk_num;
-    hdr_ptr->chunk_num = hdr.chunk_num;
-    hdr_ptr->chunk_len = hdr.chunk_len;
 }
 
 void to_sock_addr(struct address addr) {
--- sender/pi-fpv/src/communication/packet_handler.h
+++ sender/pi-fpv/src/communication/packet_handler.h
@@ -5,11 +5,20 @@
 UDP packet handling related.
 */
 
+// deprecated.
 #define HEADER_LEN 8
+
 #define PAYLOAD_LEN 1400 // header not included
 #define CHUNK_SIZE HEADER_LEN + PAYLOAD_LEN
 #define RECV_BUF_LEN 2048
 
+#define PKT_HDR_LEN 20
+#define PKT_HDR_LEN_ACK 12
+#define MSG_HDR_LEN 12
+#define CHUNK_HDR_LEN 8
+
+
+// deprecated
 /**
 * Packet header for each UDP packet.
 0                   1                   2                   3
@@ -66,7 +75,7 @@ Message Header
 - Flag:
 - Chunk Number: count the current chunk number of this message
 - Total Chunk Number: total number of chunks in this message
-- Chunk Length: Total length of this packet/chunk (header included)
+- Chunk Length: Total length of this chunk (chunk header included, but message header not)
 - Frame Sequence Number: individual sequence number of each message(frame), audio and video are separatly
 
 Chunk：Header - Video
@@ -138,7 +147,7 @@ If realtime requierd, then set flag to urgen, or use separat wireless module for
 			packet if already received one
 	- Frame Sequence Number: cooresponding frame sequence number of this frame
 	- Frame ACK: acknowlegement, report if the last frame recievd successfully
-	- Chunk Length: the whole packet length include header
+	- Chunk Length: sounds confused, but chunk length here means the whole chunk data length not include header
 	- Request Number: the number of bunded requests
 
 - Data: A serials of requests.
@@ -208,11 +217,52 @@ enum class MSG_TYPE {
 	ACK,
 	ACK_REQ,
 };
+
+// not defined yet
+enum class MSG_FLAG {
+	NONE;
+}
+
+enum class V_FMT {
+	RGB24,
+	JPEG,
+	YUV420;
+	// need add more
+}
+
+enum class A_FMT {
+	PCM,
+	// need more
+}
+
+enum class ACK_FLAG {
+	AUDIO,
+	VIDEO,
+}
+
+enum class ACK {
+	// 100% received
+	SUCCESSFUL,
+	// loss can be covered by padding
+	ACCEPTABLE,
+	// loss can not be covered.
+	FIALED,
+}
+
+enum class ACK_REQ {
+	// add network coding
+	ADD_NC,
+	// need more
+}
+
 // TODO: maybe set buffer pointer to data position and return a uint8_t *ptr.
-void unpack_msg_header(uint8_t * pkt_buffer, struct msg_header_v2 &msg_hdr);
-void unpack_chunk_video_header(uint8_t * pkt_buffer, struct chunk_header_video_v2 &chunk_hdr);
-void unpack_chunk_audio_header(uint8_t * pkt_buffer, struct chunk_header_audio_v2 &chunk_hdr);
-void unpack_packet_header(uint8_t * pkt_buffer, struct packet_header_v2 &pkd_hdr);
+
+
+void unpack_msg_header(uint8_t * pkt_buffer, struct msg_header_v2 *msg_hdr);
+void unpack_chunk_video_header(uint8_t * pkt_buffer, struct chunk_header_video_v2 *chunk_hdr);
+void unpack_chunk_audio_header(uint8_t * pkt_buffer, struct chunk_header_audio_v2 *chunk_hdr);
+void unpack_packet_header(uint8_t * pkt_buffer, struct packet_header_v2 *pkd_hdr);
+
 uint8_t get_msg_type(uint8_t * pkt_buffer);
 
 void pack_packet_header(uint8_t * pkt_buffer, struct packet_header_v2 &hdr);
@@ -222,49 +272,11 @@ parse header from a chunk.
 */
 struct chunk_header unpack_header(uint8_t * pkt_buffer, uint8_t hdr_len);
 
-
-/*
-pack chunk header to send buffer.
-*/
-void pack_header(uint8_t * buffer, struct chunk_header &hdr);
-
 struct address {
 	char *ip;
 	int port;
 };
 
 void to_sock_addr(struct address addr);
-
-/*
- 这个方案暂时不考虑使用真异步UDP，因为图传中，发送者主要以采集摄像头数据并且发送给接收者，而接收者主要是接受数据。
- 同时，考虑到一般的图传模块和控制模块是分开的，如果不是特别极限的应用场景，这种模式有点浪费资源，因为需要使用两个无线模块。
- 本方案接受和发送都使用同一个无线模块，同一个socket实体来接受和发送数据，在一般场景下是这样。万一需要用在特殊场景下，
- 也可以只保留本方案中的单一方向传输的功能，然后增加单独的控制模块，丝毫不影响兼容性。
-
- 由于UDP是无连接协议，没有像TCP那样的流控制，拥塞控制和重传机制，所以很灵活，本方案也是看中的这个特性，
- 但是众所周知现实中的网络并不是完美的，丢包是无法避免的，所以在UDP的基础上，针对图传加控制应用场景增加了acknowledge机制。
- 但是又不像TCP的acknowledge机制那么高密度，以一个数据包为单位，本方案以一帧图像或者一个h264/h265数据帧为单位，一般来说，一帧图像数据小则几十k，大则几百甚至以M为单位，
- 当开始接收到下一帧图像数据的数据包时，接收端给发送端发送反馈数据包。
- 如果只是反馈简单的成功收到一帧图像这种信息，专门用一个数据包有点浪费，所以本方案复用这个acknowledgement数据包，包含一下信息并发送给发送端：
-  1. 接收情况 - 一帧成功/失败
-  2. 网络状况 - 用 一帧图像中 接收失败/总数据包量 来计算当前网络状况比如丢包率等。
-			   如果网络状况差，也可以进行自动降低帧率清晰度等操作
-			   network coding编码会带来一定程度的延时，但是在网络状况差的时候是值得的。
-  3. 网络数据指令： 如果网络状况差，那么就开启network coding机制，由于是用于图传的方案，所以不考虑重传，某些帧丢失就丢失，毕竟发送端可以同时在本地储存，视频数据最终是完好保存下来的。
-  4. 控制指令： 这里用来发送发送端的小车，无人机或其他载有相机的装置进行控制的信息，由于这种控制信息一般都很短，所以放置在这里很合适。
-
-特殊情况：
-- 如果视频/图像一帧太大，那么acknowlegement数据包将以小于一帧图像的间隔进行。 发送反馈数据包间隔 = min（指定数量数据包， 一帧图像数据所需数据包数量）
-- 由于网络存在丢包，而控制等信息很重要，必须保证送到。所以为了保证反馈数据包送达，采用两个机制同时保证接受端能收到：
-	1. 每次连续发送2-3次，由于采集两帧图像之间有间隔，所以发送两帧数据也会有间隔，接受端接收两帧数据之间也会有间隔，所以多发两次不会有延时上的影响。
-	2. 接受端发送端图像数据包包头内记录控制信息的接收情况，对每个有效指令数据包（重复的只算一个）进行编号，如果接收到，那么就记录上一个收到的编号，放进包头发送出去
-	   接受端收到就知道需不需要重新发送。
-
-由于两帧之间时间间隔的存在，还有时间可以让发送端发送出了图像数据意外的数据，比如更新发送端设备，比如摄像头和传感器等信息发送给接受端。
-
-针对以上机制，将设计专用的数据传输协议，包括数据包头以及在代码中各个机制的实现。
-
-当然，wifi+udp只是一种传输方案，虽然不考虑TCP-like的协议，但是如同openhd使用的braodcast方式也将会进行实现，由于代码低耦合，不同传输方案和不同设备都可以方便灵活切换。
-*/
 
 #endif /*_PACKET_HANDLER_H_*/
